d549b92ff1b7858d19b1f0198d4183f992bb7ec5
ErfanDehghani <erfan.dehghani67@yahoo.com>
2024-01-09 20:56:33 +0330

    randomizing samples based on oversampling percentage and main now has tests for accuracy and f1

--- Smote.py
+++ Smote.py
@@ -11,18 +11,28 @@ class Smote:
 
         self.samples = pandas.DataFrame(samples).to_numpy()
 
-        self.overSampleAmount = over_sample_percentage / 100
+        self.overSampleAmount = over_sample_percentage
         self.nNearestNeighbors = n_nearest_neighbors + 1
 
         self.numberOfAttributes = len(self.samples[0])
 
-        self.createdSamples = 0
+        self.t = len(self.samples)
 
-        self.minorityClassSamples = samples
+        self.createdSamples = 0
 
         self.samplesNearestNeighbors = self.set_nearest_neighbors()
 
+        self.randomize_samples()
+
         self.synthetics = self.create_synthetic_samples()
+
+    def randomize_samples(self):
+        if self.overSampleAmount < 100:
+            self.t = int(self.overSampleAmount / 100 * self.t)
+            numpy.random.shuffle(self.samples)
+            self.overSampleAmount = 1
+        else:
+            self.overSampleAmount = self.overSampleAmount / 100
 
     def get_nearest_neighbors(self):
         return self.samplesNearestNeighbors[:, 1:self.nNearestNeighbors]
@@ -37,23 +47,28 @@ class Smote:
         synthetic_index = 0
         synthetic_samples = numpy.zeros((int(len(self.samples) * self.overSampleAmount), int(self.numberOfAttributes)))
 
-        for i in range(len(self.samples)):
+        for i in range(0, self.t):
 
             over_sampling_amount = self.overSampleAmount
 
             while over_sampling_amount != 0:
-                random_neighbor = random.randrange(1, self.nNearestNeighbors)
+                random_neighbor = numpy.random.choice(self.nNearestNeighbors)
 
                 for attribute in range(self.numberOfAttributes):
                     selected_nearest_neighbor = self.samplesNearestNeighbors[i][random_neighbor]
-                    gap = abs(self.samples[selected_nearest_neighbor][attribute] - self.samples[i][attribute])
-                    difference = random.uniform(0, 1)
+                    gap = self.samples[selected_nearest_neighbor][attribute] - self.samples[i][attribute]
+                    difference = numpy.random.uniform(0, 1)
                     synthetic_samples[synthetic_index][attribute] = self.samples[i][attribute] + gap * difference
 
                 synthetic_index = synthetic_index + 1
+                self.createdSamples += 1
                 over_sampling_amount = over_sampling_amount - 1
 
         return synthetic_samples
+
+    def get_created_sample_count(self):
+        return self.createdSamples
+
 
     def get_synthetic_samples(self):
         return self.synthetics
--- main.py
+++ main.py
@@ -1,10 +1,79 @@
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score
 from Smote import Smote
-import pandas
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_breast_cancer
+from sklearn.utils import check_X_y
+import numpy as np
 
-iris = load_iris()
+def get_minority_class_samples(data, target):
+    """Returns the samples belonging to the minority class in a dataset.
 
-test = Smote(pandas.DataFrame(iris.data).iloc[0:10, :], 200, 3)
+    Args:
+        data: The input data matrix (features).
+        target: The target vector (classes).
+
+    Returns:
+        A tuple containing:
+            - The minority class samples (subset of the input data).
+            - The corresponding target values for the minority class samples.
+    """
+
+    X, y = check_X_y(data, target)
+
+    unique_classes, counts = np.unique(y, return_counts=True)
+    minority_class = unique_classes[np.argmin(counts)]
+
+    mask = y == minority_class
+    minority_samples = X[mask]
+    minority_targets = y[mask]
+
+    return minority_samples, minority_targets
 
 
-print(test.get_synthetic_samples())
+data = load_breast_cancer()
+X = data.data
+y = data.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Identify the minority class
+minority_class = pd.Series(y_train).value_counts().idxmin()
+
+# Apply SMOTE only to the minority class samples
+minority_samples, minority_targets = get_minority_class_samples(X_train,y_train)
+
+smote = Smote(minority_samples, over_sample_percentage=200, n_nearest_neighbors=5)
+
+X_oversampled = smote.get_synthetic_samples()
+y_oversampled = pd.Series(minority_targets[0]).repeat(smote.get_created_sample_count())
+
+# Combine oversampled minority samples with original majority samples
+X_train_smote = pd.concat([pd.DataFrame(X_train), pd.DataFrame(X_oversampled)], ignore_index=True)
+y_train_smote = pd.concat([pd.DataFrame(y_train), y_oversampled], ignore_index=True)
+
+# Train a model with the oversampled data
+model_smote = LogisticRegression()
+model_smote.fit(X_train_smote, y_train_smote)
+y_pred_smote = model_smote.predict(X_test)
+
+normal_model = LogisticRegression()
+normal_model.fit(X_train, y_train)
+y_pred_normal = normal_model.predict(X_test)
+
+# Evaluate performance metrics on the Smoted dataset
+accuracy_smote = accuracy_score(y_test, y_pred_smote)
+f1_smote = f1_score(y_test, y_pred_smote)
+
+# Evaluate performance metrics on the normal dataset
+accuracy_normal = accuracy_score(y_test, y_pred_normal)
+f1_normal = f1_score(y_test, y_pred_normal)
+
+print("Accuracy with SMOTE (minority class only):", accuracy_smote)
+print("F1-score with SMOTE (minority class only):", f1_smote)
+
+print("Accuracy with Normal (minority class only):", accuracy_normal)
+print("F1-score with Normal (minority class only):", f1_normal)
