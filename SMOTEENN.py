e8e5226015011f651032e9978d09da6a25b35bfe
ErfanDehghani <erfan.dehghani67@yahoo.com>
2024-01-15 22:51:34 +0330

    smote-enn full implementation with imblear ENN

--- SMOTEENN.py
+++ SMOTEENN.py
@@ -1,11 +1,13 @@
-from sklearn.datasets import make_classification
+from Smote import Smote
 import numpy as np
+import pandas as pd
+from imblearn.under_sampling import EditedNearestNeighbours
 
 
 class SMOTEENN:
     VALID_STRATEGIES: set[str] = {"minority", "not minority", "not majority", "all"}
 
-    def __init__(self, x_train, y_train, over_sample_percentage, n_nearest_neighbors=3,
+    def __init__(self, x, y, over_sample_percentage, n_nearest_neighbors=3,
                  sampling_strategy='not majority'):
 
         # Checking for valid sampling strategy options and raise an Error on Wrong input
@@ -15,69 +17,127 @@ class SMOTEENN:
 
         # Initializing properties
         self.sampling_strategy = sampling_strategy
-        self.x_train = x_train
-        self.y_train = y_train
+        self.x = x
+        self.y = y
         self.over_sample_percentage = over_sample_percentage
         self.n_nearest_neighbors = n_nearest_neighbors
-        self.separated_classes = self.separate_x_train()
-        self.minority_class = self.get_minority_class()
-        self.majority_class = self.get_majority_class()
+        self.separated_x_classes = self.separate_x()
+        self.separated_y_classes = self.separate_y()
+        self.minority_samples, self.minority_targets = self.get_minority_class()
+        self.majority_samples, self.majority_targets = self.get_majority_class()
 
-    def separate_x_train(self):
-        unique_classes = np.unique(self.y_train)  # Get unique class labels
+    def separate_x(self):
+        """Separates the x samples into lists based on their corresponding class labels.
+
+        Returns:
+            dict: A dictionary where keys are the unique class labels and values are lists of x samples for each class.
+        """
+        unique_classes = np.unique(self.y)  # Get unique class labels
 
         class_data = {}
         for class_label in unique_classes:
-            mask = self.y_train == class_label
-            class_data[class_label] = self.x_train[mask]
+            mask = self.y == class_label
+            class_data[class_label] = self.x[mask]
 
         return class_data
 
-    def get_majority_class(self):
-        majority_class = self.separated_classes[0]
-        for i in range(len(self.separated_classes)):
-            if len(self.separated_classes[i]) > len(majority_class):
-                majority_class = self.separated_classes[i]
+    def separate_y(self):
+        """Separates the y labels into lists based on their corresponding class labels.
 
-        return majority_class
+        Returns:
+            dict: A dictionary where keys are the unique class labels and values are lists of y labels for each class.
+        """
+
+        unique_classes = np.unique(self.y)  # Get unique class labels
+        separated_y = {}
+
+        for class_label in unique_classes:
+            mask = self.y == class_label
+            separated_y[class_label] = self.y[mask]
+
+        return separated_y
+
+    def get_corresponding_y(self, length_of_x):
+        for i in range(len(self.separated_y_classes)):
+            if len(self.separated_y_classes[i]) == length_of_x:
+                return self.separated_y_classes[i]
+
+    def get_majority_class(self):
+        majority_class = self.separated_x_classes[0]
+        for i in range(len(self.separated_x_classes)):
+            if len(self.separated_x_classes[i]) > len(majority_class):
+                majority_class = self.separated_x_classes[i]
+
+        return majority_class, self.get_corresponding_y(len(majority_class))
 
     def get_minority_class(self):
-        minority_class = self.separated_classes[0]
-        for i in range(len(self.separated_classes)):
-            if len(self.separated_classes[i]) < len(minority_class):
-                minority_class = self.separated_classes[i]
+        minority_class = self.separated_x_classes[0]
+        for i in range(len(self.separated_x_classes)):
+            if len(self.separated_x_classes[i]) < len(minority_class):
+                minority_class = self.separated_x_classes[i]
 
-        return minority_class
+        return minority_class, self.get_corresponding_y(len(minority_class))
 
-    def get_not_majority_classes(self):
-        result_array = []
-        for i in range(len(self.separated_classes)):
-            if len(self.separated_classes[i]) != len(self.majority_class):
-                result_array.append(self.separated_classes[i])
-        return result_array
+    # def get_not_majority_classes(self):
+    #     result_array = []
+    #     for i in range(len(self.separated_x_classes)):
+    #         if len(self.separated_x_classes[i]) != len(self.majority_samples):
+    #             result_array.append(self.separated_x_classes[i])
+    #     return result_array
+    #
+    # def get_not_minority_classes(self):
+    #     result_array = []
+    #     for i in range(len(self.separated_x_classes)):
+    #         if len(self.separated_x_classes[i]) != len(self.minority_samples):
+    #             result_array.append(self.separated_x_classes[i])
+    #     return result_array
 
-    def get_not_minority_classes(self):
-        result_array = []
-        for i in range(len(self.separated_classes)):
-            if len(self.separated_classes[i]) != len(self.minority_class):
-                result_array.append(self.separated_classes[i])
-        return result_array
+    def resample(self):
+        self.smote_resample()
+        self.enn_resample()
 
-    def resample_x_train(self):
-        if self.sampling_strategy == "not majority":
-            return self.resample_not_majority_class()
+        return self.x, self.y
 
-        elif self.sampling_strategy == "not minority":
-            return self.resample_not_minority_class()
+    # def resample_not_majority_class(self):
+    #     not_majority_class = self.get_not_majority_classes()
+    #
+    # def resample_not_minority_class(self):
+    #     not_minority_class = self.get_not_minority_classes()
 
-        elif self.sampling_strategy == "minoriry":
-            return self.resample_minority_class()
+    def enn_resample(self):
+        if self.sampling_strategy == "minority":
+            self.enn_majority_resample()
 
-        elif self.sampling_strategy == "all":
-            return self.resample_all_class()
+        # elif self.sampling_strategy == "not majority":
+        #     return self.resample_not_majority_class()
+        # elif self.sampling_strategy == "not minority":
+        #     return self.resample_not_minority_Slass()
+        # elif self.sampling_strategy == "all":
+        #     return self.resample_all_class()
 
-    def resample_not_majority_class(self):
-        not_majority_class = self.get_not_majority_classes()
+    def enn_majority_resample(self):
+        enn = EditedNearestNeighbours(sampling_strategy="majority", n_neighbors=self.n_nearest_neighbors)
+        self.x, self.y = enn.fit_resample(self.x, self.y)
 
-    def resample_not_minority_class(self):
-        not_minority_class = self.get_not_minority_classes()
+    def smote_resample(self):
+        synthetic_x, synthetic_y = self.smote_oversample()
+
+        # Combining synthetic samples with the old ones
+        self.x = pd.concat([pd.DataFrame(self.x), pd.DataFrame(synthetic_x)], ignore_index=True)
+        self.y = pd.concat([pd.DataFrame(self.y), pd.DataFrame(synthetic_y)], ignore_index=True)
+
+        return self.x, self.y
+
+    # def resample_all_class(self):
+    #     self.x = self.smote_oversample()
+
+    def smote_oversample(self):
+        smote = Smote(
+            self.minority_samples,
+            over_sample_percentage=self.over_sample_percentage,
+            n_nearest_neighbors=self.n_nearest_neighbors)
+
+        synthetic_x = smote.get_synthetic_samples()
+        synthetic_y = pd.Series(self.minority_targets[0]).repeat(smote.get_created_sample_count())
+
+        return synthetic_x, synthetic_y
--- main.py
+++ main.py
@@ -1,98 +1,49 @@
-import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score, roc_curve, roc_auc_score
-from Smote import Smote
 from SMOTEENN import SMOTEENN
 from sklearn.datasets import load_breast_cancer
-from sklearn.utils import check_X_y
-import numpy as np
-from imblearn.under_sampling import EditedNearestNeighbours
+import pandas as pd
 
+# Load the dataset
+breast_cancer = load_breast_cancer()
+X = breast_cancer.data
+y = breast_cancer.target
 
-def get_minority_class_samples(data, target):
-    """Returns the samples belonging to the minority class in a dataset.
+# Check for class imbalance
+print(pd.Series(y).value_counts())
 
-    Args:
-        data: The input data matrix (features).
-        target: The target vector (classes).
+# Split into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    Returns:
-        A tuple containing:
-            - The minority class samples (subset of the input data).
-            - The corresponding target values for the minority class samples.
-    """
+smoteenn = SMOTEENN(X_train, y_train, over_sample_percentage=200, n_nearest_neighbors=3)
+X_resampled, y_resampled = smoteenn.resample()
 
-    X, y = check_X_y(data, target)
+from sklearn.linear_model import LogisticRegression
 
-    unique_classes, counts = np.unique(y, return_counts=True)
-    minority_class = unique_classes[np.argmin(counts)]
+# Train a classifier without resampling
+clf_no_resample = LogisticRegression()
+clf_no_resample.fit(X_train, y_train)
 
-    mask = y == minority_class
-    minority_samples = X[mask]
-    minority_targets = y[mask]
+# Train a classifier with resampled data
+clf_resampled = LogisticRegression()
+clf_resampled.fit(X_resampled, y_resampled)
 
-    return minority_samples, minority_targets
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
+# Evaluate performance for both models
+y_pred_no_resample = clf_no_resample.predict(X_test)
+y_pred_resampled = clf_resampled.predict(X_test)
 
-data = load_breast_cancer()
-X = data.data
-y = data.target
+print("Accuracy (no resampling):", accuracy_score(y_test, y_pred_no_resample))
+print("Accuracy (resampled):", accuracy_score(y_test, y_pred_resampled))
 
+print("Precision (no resampling):", precision_score(y_test, y_pred_no_resample))
+print("Precision (resampled):", precision_score(y_test, y_pred_resampled))
 
-smoteenn = SMOTEENN(X, y, 200, 3, "not majority")
-print(len(smoteenn.resample_x_train()))
-print(len(X))
+print("Recall (no resampling):", recall_score(y_test, y_pred_no_resample))
+print("Recall (resampled):", recall_score(y_test, y_pred_resampled))
 
+print("F1-score (no resampling):", f1_score(y_test, y_pred_no_resample))
+print("F1-score (resampled):", f1_score(y_test, y_pred_resampled))
 
-# Split the dataset into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=86)
-
-
-# # Apply SMOTE only to the minority class samples
-# minority_samples, minority_targets = get_minority_class_samples(X_train, y_train)
-#
-# smote = Smote(minority_samples, over_sample_percentage=100, n_nearest_neighbors=2)
-#
-# X_oversampled = smote.get_synthetic_samples()
-# y_oversampled = pd.Series(minority_targets[0]).repeat(smote.get_created_sample_count())
-#
-# # Combine oversampled minority samples with original majority samples
-# X_train_smote = pd.concat([pd.DataFrame(X_train), pd.DataFrame(X_oversampled)], ignore_index=True)
-# y_train_smote = pd.concat([pd.DataFrame(y_train), y_oversampled], ignore_index=True)
-#
-# # Train a model with the oversampled data
-# model_smote = LogisticRegression()
-# model_smote.fit(X_train_smote, y_train_smote)
-# y_pred_smote = model_smote.predict(X_test)
-#
-# normal_model = LogisticRegression()
-# normal_model.fit(X_train, y_train)
-# y_pred_normal = normal_model.predict(X_test)
-#
-# # Evaluate performance metrics on the Smoted dataset
-# accuracy_smote = accuracy_score(y_test, y_pred_smote)
-# f1_smote = f1_score(y_test, y_pred_smote)
-#
-# # Evaluate performance metrics on the normal dataset
-# accuracy_normal = accuracy_score(y_test, y_pred_normal)
-# f1_normal = f1_score(y_test, y_pred_normal)
-#
-# print("Accuracy with SMOTE (minority class only):", accuracy_smote)
-# print("F1-score with SMOTE (minority class only):", f1_smote)
-#
-# print("Accuracy with Normal:", accuracy_normal)
-# print("F1-score with Normal:", f1_normal)
-#
-#
-# # Evaluate model on the original test set (without SMOTE)
-# y_true = y_test
-# y_pred = y_pred_smote  # Use predictions from the model trained with SMOTE
-#
-# # Generate ROC curve and AUC
-# fpr, tpr, thresholds = roc_curve(y_true, y_pred)
-# roc_auc = roc_auc_score(y_true, y_pred)
-#
-# print("Number of Minority Class targets: ", len(minority_targets), "\nNumber of Minority Class samples: ", len(minority_samples))
-# print("Number of targets after oversampling: ", len(y_oversampled), "\nNumber of samples after oversampling: ", len(X_oversampled))
-# print("Number of smote_y_train: ", len(y_train_smote), "\nNumber of smote_x_train: ", len(X_train_smote))
+print("ROC AUC (no resampling):", roc_auc_score(y_test, y_pred_no_resample))
+print("ROC AUC (resampled):", roc_auc_score(y_test, y_pred_resampled))
